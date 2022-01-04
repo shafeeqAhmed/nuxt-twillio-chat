ee6b5f80c0b7f91739672693217015a293d7197c
Talha Fayyaz <talha.fayyaz52@gmail.com>
2022-01-04 11:59:08 +0500

    Revert "after merrage code"
    This reverts commit 74ca450aaf84411545a966ab57c165743f9658c0.

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,8 +74,8 @@ export default {
     [
       '@nuxtjs/laravel-echo', {
         broadcaster: 'pusher',
-        key: '3fef7a0ccafd37cf4a23',
-        cluster: 'ap2',
+        key: '7d65294556a4568cae7f',
+        cluster: 'mt1',
         forceTLS: true
       }
     ]
@@ -107,7 +107,8 @@ export default {
     }
   },
   axios: {
-    baseURL: 'https://text-app.tkit.co.uk/api/api'
+
+      baseURL: 'http://localhost/twillo-api/api'
     },
   auth: {
     redirect: {

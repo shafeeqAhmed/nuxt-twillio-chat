49f14d7d2774b802666c82db122dc0e91f6611e2
shafeeqAhmed <shafeeq.ahmed541@gmail.com>
2022-04-08 23:36:08 +0500

    final feedback

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -3,26 +3,24 @@
  * Auth-layout
  */
 export default {
-    data() {
-        return {
-
-        }
-    },
-  middleware: 'nonAuthentication'
-}
+  data() {
+    return {};
+  },
+  middleware: "nonAuthentication",
+};
 </script>
 
 <template>
-<div>
+  <div>
     <div class="account-pages mt-5 mb-5">
-        <div class="container">
-            <Nuxt />
-        </div>
-        <!-- end container -->
+      <div class="container">
+        <Nuxt />
+      </div>
+      <!-- end container -->
     </div>
     <!-- end page -->
     <footer class="footer footer-alt">
-        {{new Date().getFullYear()}} &copy; Develop By <a href="">Oria Soft</a>
+      {{ new Date().getFullYear() }} &copy;Colony
     </footer>
-</div>
+  </div>
 </template>
